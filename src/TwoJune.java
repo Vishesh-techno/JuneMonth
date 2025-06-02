3a214193772141843167bb444e50dcbf39b54739
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-02 15:32:07 +0530

    Revision

--- src/TwoJune.java
+++ src/TwoJune.java
@@ -20,13 +20,36 @@ public class TwoJune {
         }
         return arr;
     }
+    public static int maxWealth(int[][] arr) {
+        int cs = 0;
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            cs = 0;
+            for(int j = 0; j<arr[i].length; j++){
+                cs = cs + arr[i][j];
+            }
+            max = Math.max(max, cs);
+        }
+        return max;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[][] twodim = new int[3][3];
+        for (int i = 0; i < twodim.length; i++) {
+          for (int j = 0; j < twodim[i].length; j++) {
+              twodim[i][j] = sc.nextInt();
+          }
+        }
+        for (int i = 0; i < twodim.length; i++) {
+            System.out.println(Arrays.toString(twodim[i]));
+        }
         int[] nums = new int[10];
         for (int i = 0; i < nums.length; i++) {
            nums[i] = i;
         }
         System.out.println("The Concatenated Array is: "+Arrays.toString(conctenateArray(nums)));
         System.out.println("The Running ArraysSum is: "+Arrays.toString(RunningSum(nums)));
+        System.out.println("The Max Wealth is: "+maxWealth(twodim));
     }
 }
