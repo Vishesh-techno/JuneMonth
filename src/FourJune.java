f3f08b554224aecd076d01586cc9641dfaa0a281
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-04 17:30:44 +0530

    Revision

--- src/FourJune.java
+++ src/FourJune.java
@@ -23,8 +23,40 @@ public class FourJune {
 
         return result;
     }
+    public static List<List<Integer>> threeSumOptimal(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); // Step 1: sort the array
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = nums.length - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
         System.out.println(threeSum(nums));
+        System.out.println(threeSumOptimal(nums));
     }
 }
