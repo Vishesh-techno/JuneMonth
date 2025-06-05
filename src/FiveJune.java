dba2ce6f4609da56cd6375c4774a8007a34babfa
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-05 20:20:52 +0530

    Revision

--- src/FiveJune.java
+++ src/FiveJune.java
@@ -1,12 +1,30 @@
-import java.util.Arrays;
+import java.util.*;
+
 public class FiveJune {
+    public static int[] createTargetArrayOptimal(int[] nums, int[] index) {
+        List<Integer> target = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (index[i] >= target.size()) {
+                target.add(nums[i]);
+            } else {
+                target.add(index[i], nums[i]);
+            }
+        }
+
+        int[] result = new int[target.size()];
+        for (int i = 0; i < target.size(); i++) {
+            result[i] = target.get(i);
+        }
+
+        return result;
+    }
+
     public static int[] createTargetArray(int[] nums, int[] index) {
         int n = nums.length;
         int[] target = new int[n];
         int size = 0;
 
         for (int i = 0; i < n; i++) {
-            // Shift elements to right from index[i] to size-1
             for (int j = size; j > index[i]; j--) {
                 target[j] = target[j - 1];
             }
@@ -19,6 +37,8 @@ public class FiveJune {
     public static void main(String[] args) {
         int[] nums = {2, 4, 5, 6, 7, 5};
         int[] index = {0, 1, 3, 4, 5, 2};
-        System.out.println(Arrays.toString(createTargetArray(nums, index)));
+
+        System.out.println(Arrays.toString(createTargetArray(nums.clone(), index.clone())));
+        System.out.println(Arrays.toString(createTargetArrayOptimal(nums.clone(), index.clone())));
     }
 }
