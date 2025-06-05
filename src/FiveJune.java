808ca1f1e8c34dd18be9e587cd007fcf3e82cf3c
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-05 20:02:07 +0530

    Revision

--- src/FiveJune.java
+++ src/FiveJune.java
@@ -2,24 +2,23 @@ import java.util.Arrays;
 public class FiveJune {
     public static int[] createTargetArray(int[] nums, int[] index) {
         int n = nums.length;
-        int m = index.length;
         int[] target = new int[n];
-        for(int i = 0; i<n; i++){
-            if(index[i] == i){
-                target[index[i]] = nums[i];
-            }else{
-                for(int j=index[i]; j<i+1; j++){
-                    int temp = target[j];
-                    target[j] = nums[i];
-                    nums[i] = temp;
-                }
+        int size = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Shift elements to right from index[i] to size-1
+            for (int j = size; j > index[i]; j--) {
+                target[j] = target[j - 1];
             }
+            target[index[i]] = nums[i];
+            size++;
         }
         return target;
     }
+
     public static void main(String[] args) {
-        int[] nums = {2,4,5,6,7,5};
-        int[] index = {0,1,3,4,5,2};
+        int[] nums = {2, 4, 5, 6, 7, 5};
+        int[] index = {0, 1, 3, 4, 5, 2};
         System.out.println(Arrays.toString(createTargetArray(nums, index)));
     }
 }
