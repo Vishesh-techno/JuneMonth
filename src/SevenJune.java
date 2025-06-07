37561f6dde35afb030274d3087f89cc8a3243ddc
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-07 16:50:03 +0530

    Revision

--- src/SevenJune.java
+++ src/SevenJune.java
@@ -19,11 +19,22 @@ public class SevenJune {
         }
         return count;
     }
+    public static int findNum(int[] arr){
+        int ans = 0;
+        for(int i=0; i<arr.length; i++){
+            if(String.valueOf(arr[i]).length() % 2 == 0 ){
+                ans++;
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
+        int[]  nums = {2,5,48,9,7,6,52,55225,4,5,2,456,321,165,4321,32,1,3};
         int[][] indices = {{0,1},{1,1}};
         int n = 2, m = 3;
         int res = oddCells(n, m, indices);
         System.out.println(res);
+        System.out.println(findNum(nums));
 
     }
 }
