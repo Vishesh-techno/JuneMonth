61fdaa56c85a0ce1f91e92762135ea84f5265667
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-09 19:22:03 +0530

    Revision

---
+++ src/NineJune.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class NineJune {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int[] ans = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] + 1 != 10) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        ans[0] = 1;
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {9, 9, 9, 9, 9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(nums)));
+    }
+}
