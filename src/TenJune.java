9d8d21dc4d7fdcb8a0e785f7a71abf29401d2f2b
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-10 20:54:37 +0530

    Revision

--- src/TenJune.java
+++ src/TenJune.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TenJune {
     public static int maximumWealth(int[][] accounts) {
         int cs = 0;
@@ -12,8 +14,25 @@ public class TenJune {
         }
         return max;
     }
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            int sm = 0;
+            for(int j=0; j<n; j++){
+                if(nums[i] > nums[j]){
+                    sm++;
+                }
+            }
+            ans[i] = sm;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[][] accounts = {{1,1,1,2,2,2},{2,2,2,23,3,3}};
+        int[] array = {8,1,2,2,3};
         System.out.println(maximumWealth(accounts));
+        System.out.println(maximumWealth(accounts));
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(array)));
     }
 }
