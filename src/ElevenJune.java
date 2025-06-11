e9b571c9cd07465e8e44ce112008e9312b352cba
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-11 18:32:25 +0530

    Revision

--- src/ElevenJune.java
+++ src/ElevenJune.java
@@ -8,6 +8,22 @@ public class ElevenJune {
         }
         return cs;
     }
+    public static int maximum(int[] arr){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i =0 ; i<arr.length; i++){
+           if(arr[i] < min){
+               min = arr[i];
+           }
+           else if(arr[i] > max){
+               max = arr[i];
+           }
+        }
+        System.out.println(min+" is minimum from array");
+        System.out.println(max+" is maximum from array");
+        return max;
+    }
+
     public static void main(String[] args) {
         int[]  arr = new int[10];
         for(int i=0;i<arr.length;i++){
@@ -17,7 +33,9 @@ public class ElevenJune {
             System.out.print(arr[i]+" ");
         }
         System.out.println();
-        System.out.println("the "+ maxSum(arr));
+        System.out.println("the sum of all the elements in array is: "+ maxSum(arr));
+        System.out.println(maximum(arr));
+
 
     }
 }
