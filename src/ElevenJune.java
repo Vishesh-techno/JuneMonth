d3d0fc1b9b2e7b5131127a207990b3a88cabdd2a
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-11 19:32:48 +0530

    Revision

--- src/ElevenJune.java
+++ src/ElevenJune.java
@@ -37,9 +37,36 @@ public class ElevenJune {
         }
         return ans;
     }
+//    public static int bestTimeForSell(int[] arr){
+//        int n = arr.length;
+//        int min = Integer.MAX_VALUE;
+//        int max = Integer.MIN_VALUE;
+//
+//    }
+    public static int maximumSum(int[] arr){
+        int cs = 0;
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        int maxElement = Integer.MIN_VALUE;
+        for(int i =0; i<n; i++){
+            cs = cs + arr[i];
+            if(cs < 0){
+                cs = 0;
+            }
+            max = Math.max(max, cs);
+            if(arr[i] > maxElement){
+                maxElement = arr[i];
+            }
+        }
+        if(max == 0 && maxElement < 0){
+            return maxElement;
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
         int[]  arr = new int[10];
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         for(int i=0;i<arr.length;i++){
             arr[i] = i + 1;
         }
@@ -50,6 +77,8 @@ public class ElevenJune {
         System.out.println("the sum of all the elements in array is: "+ maxSum(arr));
         System.out.println(maximum(arr));
         System.out.println("the sum of target indexes is: "+ Arrays.toString(twoSum(arr, 9)));
+        System.out.println("the maximum sum of sub array is: "+ maximumSum(arr));
+        System.out.println("the maximum sum of sub array is: "+ maximumSum(nums));
 
 
     }
