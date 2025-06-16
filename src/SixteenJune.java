d8c701b411ca1999aceff0fcedcb5b36bfb50434
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-16 18:32:21 +0530

    Revision

--- src/SixteenJune.java
+++ src/SixteenJune.java
@@ -9,8 +9,17 @@ public class SixteenJune {
         }
         return ans;
     }
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n*2];
+        for(int i=0; i<n; i++){
+            ans[i]=ans[i+n]=nums[i];
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] nums = {0,2,1,5,3,4};
         System.out.println(Arrays.toString(permutation(nums)));
+        System.out.println(Arrays.toString(getConcatenation(nums)));
     }
 }
