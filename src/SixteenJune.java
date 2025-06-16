a155ca33b245ff40f7023e1583a7e4f13319e7f0
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-16 18:34:23 +0530

    Revision

--- src/SixteenJune.java
+++ src/SixteenJune.java
@@ -1,24 +1,26 @@
 import java.util.Arrays;
 
 public class SixteenJune {
-    public static int[] permutation(int[] nums){
+    public static int[] permutation(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             ans[i] = nums[nums[i]];
         }
         return ans;
     }
+
     public static int[] getConcatenation(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n*2];
-        for(int i=0; i<n; i++){
-            ans[i]=ans[i+n]=nums[i];
+        int[] ans = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            ans[i] = ans[i + n] = nums[i];
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int[] nums = {0,2,1,5,3,4};
+        int[] nums = {0, 2, 1, 5, 3, 4};
         System.out.println(Arrays.toString(permutation(nums)));
         System.out.println(Arrays.toString(getConcatenation(nums)));
     }
