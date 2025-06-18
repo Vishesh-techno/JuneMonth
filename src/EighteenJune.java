35bea8e12b0a37447b59faa2aab8512046a7e599
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-18 16:59:40 +0530

    Revision

--- src/EighteenJune.java
+++ src/EighteenJune.java
@@ -26,8 +26,48 @@ public class EighteenJune {
 
         return res;
     }
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int rowStart = 0, rowEnd = n - 1;
+        int colStart = 0, colEnd = n - 1;
+        int num = 1;
+
+        while (num <= n * n) {
+            // Traverse right
+            for (int i = colStart; i <= colEnd; i++) {
+                matrix[rowStart][i] = num++;
+            }
+            rowStart++;
+
+            // Traverse down
+            for (int i = rowStart; i <= rowEnd; i++) {
+                matrix[i][colEnd] = num++;
+            }
+            colEnd--;
+
+            // Traverse left
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    matrix[rowEnd][i] = num++;
+                }
+            }
+            rowEnd--;
+
+            // Traverse up
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    matrix[i][colStart] = num++;
+                }
+            }
+            colStart++;
+        }
+
+        return matrix;
+    }
     public static void main(String[] args) {
         int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        int n = 4;
         System.out.println(spiralOrder(mat));
+        System.out.println(Arrays.deepToString(generateMatrix(n)));
     }
 }
