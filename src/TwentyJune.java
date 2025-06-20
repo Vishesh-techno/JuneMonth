f45a6b74f935180cf56207d515ce43429b3dd479
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-20 19:51:07 +0530

    Revision

--- src/TwentyJune.java
+++ src/TwentyJune.java
@@ -3,29 +3,30 @@ import java.util.*;
 import java.util.Scanner;
 
 public class TwentyJune {
-    public static List<Integer> addToArray(int[] nums, int k){
-        int i = nums.length-1;
+    public static List<Integer> addToArray(int[] nums, int k) {
+        int i = nums.length - 1;
         List<Integer> ans = new ArrayList<>();
-        while(i>=0||k>0){
-            if(i>=0){
-                ans.add((nums[i] + k)%10);
-                k=(nums[i]+k)/10;
-            }else{
-                ans.add(k%10);
-                k = k/10;
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                ans.add((nums[i] + k) % 10);
+                k = (nums[i] + k) / 10;
+            } else {
+                ans.add(k % 10);
+                k = k / 10;
             }
             i--;
         }
         Collections.reverse(ans);
         return ans;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array: ");
         int n = sc.nextInt();
         System.out.println("Enter the array: ");
         int[] num = new int[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             num[i] = sc.nextInt();
         }
         System.out.println(Arrays.toString(num));
