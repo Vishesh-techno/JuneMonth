f65f93a8e0dd9546c38cc82c57a217edacb1df6b
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-23 21:22:13 +0530

    Revision

--- src/TwentyThreeJune.java
+++ src/TwentyThreeJune.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TwentyThreeJune {
     public static boolean search(String str, char target){
        if(str.length()==0){
@@ -9,6 +11,21 @@ public class TwentyThreeJune {
            }
        }
        return false;
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left <= right) {
+            int tot = numbers[left] + numbers[right];
+            if (tot == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (tot > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[] { -1, -1 };
     }
     public static int guessNumber(int n, int guess){
         int start = 0;
@@ -26,6 +43,7 @@ public class TwentyThreeJune {
         return -1;
     }
     public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
         String name = "Vishesh";
         char target = 'e';
         if(search(name, target)){
@@ -35,5 +53,6 @@ public class TwentyThreeJune {
         }
         System.out.println(search(name, target));
         System.out.println(guessNumber(500, 45));
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
     }
 }
