75610036f63e360dce17e1add2febcce496c7c56
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-24 21:16:00 +0530

    Revision

--- src/TwentyFourJune.java
+++ src/TwentyFourJune.java
@@ -33,10 +33,30 @@ public class TwentyFourJune {
         }
         return false;
     }
+    public static boolean isPerfectSquareOptimal(int num){
+        long start = 2;
+        long end = num/2;
+        if(num<2){
+            return true;
+        }
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            long square = mid*mid;
+            if(square==num){
+                return true;
+            } else if (square<num) {
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         int[] nums = {3, 4, 9, 1, 3, 9, 5};
         System.out.println(findKDistantIndices(nums, 9, 1));
         System.out.println(isPerfectSquare(16));
+        System.out.println(isPerfectSquareOptimal(100000001));
     }
 }
