8d830a0b44d8468ed793e2cb08655b6cd4502940
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-25 16:00:01 +0530

    Revision

--- src/TwentyFiveJune.java
+++ src/TwentyFiveJune.java
@@ -12,7 +12,34 @@ public class TwentyFiveJune {
         }
         return (int)start-1;
     }
+    public static char nextGreatestLetterOptimal(char[] letters, char target){
+        for (int i = 0; i < letters.length; i++) {
+            if(target<=letters[i]){
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+    public static char nextGreatestLetter(char[] letters, char target){
+        int start = 0;
+        int end = letters.length-1;
+        if(target>=letters[end]){
+            return letters[0];
+        }
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(letters[mid]<=target){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return letters[end];
+    }
     public static void main(String[] args) {
         System.out.println(arrangeCoins(10));
+        char[] lett = {'a','b','z'};
+        System.out.println(nextGreatestLetter(lett, 'a'));
+        System.out.println(nextGreatestLetterOptimal(lett, 'z'));
     }
 }
