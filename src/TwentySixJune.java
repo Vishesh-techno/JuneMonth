7904106ebd2385661c0bc4e5d3fc64284794145c
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-26 21:14:29 +0530

    Revision

--- src/TwentySixJune.java
+++ src/TwentySixJune.java
@@ -44,7 +44,24 @@ public class TwentySixJune {
         }
         return arr[start%arr.length];
     }
-
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        if(target>nums[end]){
+            return end+1;
+        }
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return end;
+    }
     public static void main(String[] args) {
         int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
         char [] letters = {'c','f','j'};
@@ -54,5 +71,6 @@ public class TwentySixJune {
         System.out.println("The index of ceiling number is: "+ceilNumber(arr, targetNumber));
         System.out.println("The index of floor number is: "+floorNumber(arr, targetNumber));
         System.out.println("the index of floor character is: "+ nextGreatestLargest(letters,targetLetter));
+        System.out.println("The index of Searching Insertion number is: "+searchInsert(arr, targetNumber));
     }
 }
