f4a2cb1c8cb48b49ec6e99d6df0c2f0664d96719
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-26 18:32:22 +0530

    Revision

--- src/TwentySixJune.java
+++ src/TwentySixJune.java
@@ -29,12 +29,30 @@ public class TwentySixJune {
         }
         return end;
     }
+    public static char nextGreatestLargest(char[] arr, char target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]==target){
+                return arr[mid];
+            } else if (arr[mid]<target) {
+                start = mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return arr[start%arr.length];
+    }
 
     public static void main(String[] args) {
         int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
-        int target = 62;
+        char [] letters = {'c','f','j'};
+        char targetLetter = 'a';
+        int targetNumber = 62;
         System.out.println("The length of array is: "+arr.length);
-        System.out.println("The index of ceiling number is: "+ceilNumber(arr, target));
-        System.out.println("The index of floor number is: "+floorNumber(arr, target));
+        System.out.println("The index of ceiling number is: "+ceilNumber(arr, targetNumber));
+        System.out.println("The index of floor number is: "+floorNumber(arr, targetNumber));
+        System.out.println("the index of floor character is: "+ nextGreatestLargest(letters,targetLetter));
     }
 }
