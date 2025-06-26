1b6e2b1c26f3cd707f6220e992b3a42b66721e73
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-26 17:52:18 +0530

    Revision

---
+++ src/TwentySixJune.java
@@ -0,0 +1,24 @@
+public class TwentySixJune {
+    public static int ceilNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 62;
+        System.out.println("The length of array is: "+arr.length);
+        System.out.println("The index of ceiling number is: "+ceilNumber(arr, target));
+    }
+}
